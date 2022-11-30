39b94c843f5f03f5023bd5cb9c6fe8a15b25de1b
Emma Bang <83577644+emmabang-dat@users.noreply.github.com>
2022-11-30 21:58:52 +0100

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -12,27 +12,25 @@ export default function App() {
     { name: 'Nicolai', id: '7'},
   ]);
 
-
-  /* return (
-    <View style={styles.container}>
-      <ScrollView>
-      { people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View> 
-  ); */
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id);
+    })
+  }
 
   return (
     <View style={styles.container}>
+
       <FlatList 
         numColumns={2}
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({item}) => (
-          <Text style={styles.item}>{item.name}</Text>)}/>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}/>
     </View>
   )
 }
